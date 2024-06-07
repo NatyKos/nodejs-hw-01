2067c9f1810c6c8774a090d17e368ca7e4695b73
NatyKos <145201378+NatyKos@users.noreply.github.com>
2024-06-07 19:11:48 +0100

    .

--- src/scripts/addOneContact.js
+++ src/scripts/addOneContact.js
@@ -2,19 +2,19 @@ import { PATH_DB } from '../constants/contacts.js';
 import fs from 'fs/promises';
 import { createFakeContact } from './generateContacts.js';
 
-// export const addOneContact = async () => {
-//   try {
-//     const newContacts = [];
-//     newContacts.push(createFakeContact());
-//     const currentContacts = JSON.parse(await fs.readFile(PATH_DB, 'utf-8'));
-//     const updateContacts = currentContacts.push(...newContacts);
-//     await fs.writeFile(PATH_DB, JSON.stringify(updateContacts, null, 2));
-//   } catch (error) {
-//     console.log(error);
-//   }
-// };
+export const addOneContact = async () => {
+  const newArray = [];
+  newArray.push(createFakeContact());
+  try {
+    const currentContacts = JSON.parse(await fs.readFile(PATH_DB, 'utf-8'));
+    currentContacts.push(...newArray);
+    await fs.writeFile(PATH_DB, JSON.stringify(currentContacts, null, 2));
+  } catch (error) {
+    console.log(error);
+  }
+};
 
-// await addOneContact();
+await addOneContact();
 
 // npm run add-one
 
@@ -25,15 +25,15 @@ import { createFakeContact } from './generateContacts.js';
 // Перевірити роботу функції можна виконавши команду npm run add - one,
 //     яка буде виконувати код у файлі src / scripts / addOneContact.js.
 //     У файлі src / db / db.json мають відбутися відповідні зміни.
-export const addOneContact = async () => {
-  try {
-    const contacts = await fs.readFile(PATH_DB, 'utf-8');
-    const newContact = createFakeContact();
-    const updatedContacts = [...JSON.parse(contacts), newContact];
-    await fs.writeFile(PATH_DB, JSON.stringify(updatedContacts, null, 2));
-    console.log(`Generated new contact and added new contact to the array.`);
-  } catch (error) {
-    console.error(`Error generating new contact: ${error.message}`);
-  }
-};
-await addOneContact();
+// export const addOneContact = async () => {
+//   try {
+//     const contacts = await fs.readFile(PATH_DB, 'utf-8');
+//     const newContact = createFakeContact();
+//     const updatedContacts = [...JSON.parse(contacts), newContact];
+//     await fs.writeFile(PATH_DB, JSON.stringify(updatedContacts, null, 2));
+//     console.log(`Generated new contact and added new contact to the array.`);
+//   } catch (error) {
+//     console.error(`Error generating new contact: ${error.message}`);
+//   }
+// };
+// await addOneContact();
