42ff1a026ec478be633e14aa614cceceecc51775
NatyKos <145201378+NatyKos@users.noreply.github.com>
2024-06-07 12:33:51 +0100

    generateContacts-done

--- src/scripts/generateContacts.js
+++ src/scripts/generateContacts.js
@@ -1,5 +1,26 @@
 import { PATH_DB } from '../constants/contacts.js';
+import { faker } from '@faker-js/faker';
+import fs from 'fs/promises';
 
-const generateContacts = async (number) => {};
+const createFakeContact = () => ({
+  name: faker.person.fullName(),
+  phoneNumber: faker.phone.number(),
+  email: faker.internet.email(),
+  job: faker.person.jobTitle(),
+});
+
+const generateContacts = async (number) => {
+  let newContacts = [];
+  for (let i = 0; i < number; i++) {
+    newContacts.push(createFakeContact());
+  }
+  try {
+    const contacts = JSON.parse(await fs.readFile(PATH_DB, 'utf-8'));
+    contacts.push(...newContacts);
+    await fs.writeFile(PATH_DB, JSON.stringify(contacts, null, 2));
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 await generateContacts(5);
